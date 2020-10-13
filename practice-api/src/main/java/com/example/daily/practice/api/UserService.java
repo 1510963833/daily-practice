c348c1e417ff3d7d4ba2cd6f0a675f24d6001afc
王国兴 <wangguoxing@ifugle.com>
2020-10-13 15:18:46 +0800

    新增：初始化项目信息

---
+++ practice-api/src/main/java/com/example/daily/practice/api/UserService.java
@@ -0,0 +1,14 @@
+package com.example.daily.practice.api;
+
+
+import com.example.daily.practice.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
